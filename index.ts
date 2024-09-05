1b9e8a195da40d581fba97b80e6331220fc09a60
marlon-erler <viridian035@gmail.com>
2024-09-04 22:00:51 -0700

    remove index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { ConfirmationButton } from "./Components/confirmationButton";
-export { Modal } from "./Components/modal";
-export { Popover } from "./Components/popover";
-export { ProgressBar } from "./Components/progressBar";
-export { Select } from "./Components/select";
-export { Slider } from "./Components/slider";
-export { SplitView } from "./Components/splitView";
